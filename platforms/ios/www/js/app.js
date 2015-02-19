4396bac5092031bd7e5f17949a2130b25559c3db
mstivali <mstivali@mail.usf.edu>
2015-02-18 23:06:48 -0500

    Fixed states for styles and style detail
    Also added iOS project to build

---
+++ platforms/ios/www/js/app.js
@@ -0,0 +1,82 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'akoenig.deckgrid'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
+  })
+
+  .state('app.dashboard', {
+    url: "/dashboard",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/dashboard.html"
+      }
+    }
+  })
+
+  .state('app.inventory', {
+    url: "/inventory",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/inventory.html"
+      }
+    }
+  })
+    .state('app.models', {
+      url: "/models",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/models.html",
+          controller: 'ModelsController'
+        }
+      }
+    })
+
+  .state('app.styles', {
+    url: "/models/:modelId",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/styles.html",
+        controller: 'StylesController'
+      }
+    }
+  })
+
+  .state('app.style-details', {
+    url: "/styles/:styleId",
+    views: {
+      'menuContent' : {
+        templateUrl : "templates/style-details.html",
+        controller : "StyleDetailController"
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/dashboard');
+});
