07794c2c15e9154d0d484baf8deb82286772867c
mstivali <user594@C1Banks-MacBook-Pro.local>
2015-04-06 15:05:14 -0400

    Added Inventory List to inventory controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -319,88 +319,14 @@ angular.module('starter.controllers', [])
 })
 
 .controller("InventoryController", 
-  function($scope, $ionicPopover, $ionicModal, $ionicPopup, $timeout) {
+  function($scope, $http, $ionicPopover, $ionicModal, $ionicPopup, $timeout) {
 
-  // .fromTemplateUrl() method
-  $ionicPopover.fromTemplateUrl('templates/popover-template.html', {
-    scope: $scope,
-  }).then(function(popover) {
-    $scope.popover = popover;
-  });
-
-
-  $scope.openPopover = function($event) {
-    $scope.popover.show($event);
-  };
-  $scope.closePopover = function() {
-    $scope.popover.hide();
-  };
-  //Cleanup the popover when we're done with it!
-  $scope.$on('$destroy', function() {
-    $scope.popover.remove();
-  });
-  // Execute action on hide popover
-  $scope.$on('popover.hidden', function() {
-    // Execute action
-  });
-  // Execute action on remove popover
-  $scope.$on('popover.removed', function() {
-    // Execute action
-  });
-
-  //modal
-  $ionicModal.fromTemplateUrl('templates/modal-template.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  $scope.openTestModal = function() {
-    $scope.modal.show();
-  };
-  $scope.closeTestModal = function() {
-    $scope.modal.hide();
-  };
-  //Cleanup the modal when we're done with it!
-  $scope.$on('$destroy', function() {
-    $scope.modal.remove();
-  });
-  // Execute action on hide modal
-  $scope.$on('modal.hidden', function() {
-    // Execute action
-  });
-  // Execute action on remove modal
-  $scope.$on('modal.removed', function() {
-    // Execute action
-  });
-
-
-  //popup
-  $scope.showConfirm = function() {
-   var confirmPopup = $ionicPopup.confirm({
-     title: 'Consume Ice Cream',
-     template: 'Are you sure you want to eat this ice cream?'
-   });
-   confirmPopup.then(function(res) {
-     if(res) {
-       console.log('You are sure');
-     } else {
-       console.log('You are not sure');
-     }
-   });
- };
-
-  $scope.showAlert = function() {
-   var alertPopup = $ionicPopup.alert({
-     title: 'Don\'t eat that!',
-     template: 'It might taste good'
-   });
-   alertPopup.then(function(res) {
-     console.log('Thank you for not eating my delicious ice cream cone');
-   });
- };
-
+      $http({
+        url: 'http://tvts.azurewebsites.net/api/vehicles', 
+        method: "GET",
+      }).success(function(data){
+         $scope.vehicles = data;
+      });
 });
 
 
